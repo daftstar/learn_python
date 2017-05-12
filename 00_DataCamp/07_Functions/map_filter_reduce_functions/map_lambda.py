b25a8fcc9b348758b071e802974e1a2cdcba65f9
ndaftary <nikdaftary@gmail.com>
2017-05-11 19:57:26 -0700

    map, filter & reduce functions

---
+++ 00_DataCamp/07_Functions/map_filter_reduce_functions/map_lambda.py
@@ -0,0 +1,15 @@
+# Map applies a function over an object, such as a list.
+
+nums = [2, 4, 6, 8, 10]
+result = map(lambda a: a ** 2, nums)
+
+# print as a list, otherwise returns as a <map object at 0x102b34550>
+print (list(result))
+
+
+
+spells = ["protego", "accio", "expecto patronum", "legilimens"]
+# use map() to apply lambda function over spells: shout_spells
+
+shout_spells = map(lambda item: item + "!!!", spells)
+shout_spells_list = (list(shout_spells))
